afc64292d4c6f9ce6bdcf70d60f34b5bf43f4e86
nik-lazer <nick.lazarev@gmail.com>
2019-10-31 09:48:25 +0300

    Spock test is added

---
+++ src/test/groovy/lan/test/micronaut/HelloSpec.groovy
@@ -0,0 +1,12 @@
+package lan.test.micronaut
+
+import spock.lang.Specification
+
+
+class HelloSpec extends Specification {
+
+    def "Test A"() {
+        // Always fail
+        expect: false
+    }
+}
